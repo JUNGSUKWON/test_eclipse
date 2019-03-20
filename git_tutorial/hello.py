7fd1ecb25841601c07ef29b953a8b04e7b86a006
nameok kim <djqkdaos@gmail.com>
2019-03-20 11:12:27 +0900

    conflict resolved github

--- git_tutorial/hello.py
+++ git_tutorial/hello.py
@@ -1,11 +1,11 @@
 //for command line git tutorial
 //github modification
-
+//second local git push
 print("hello")
 print("hello111")
 print("hello222")
 print("hello333")
-<<<<<<< HEAD
 
-=======
->>>>>>> origin/master
+
+
+
